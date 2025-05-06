fd22412fdb7dbed5b99ee5984d3020b6ef506e30
andreuu27 <7andreu1994@gmail.com>
2025-05-06 21:00:39 +0200

    modules, routes,etc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { CardComponent } from "./card/card.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { CardComponent } from "./shared/card/card.component";
 import { CommonModule } from '@angular/common';
-import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SearchBarComponent } from './shared/search-bar/search-bar.component';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 export interface Character {
   name: string;
@@ -10,7 +12,7 @@ export interface Character {
 } 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, CardComponent, CommonModule, SearchBarComponent],
+  imports: [HeaderComponent, CardComponent, CommonModule, SearchBarComponent, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -22,6 +24,19 @@ characters: Character[] = [
   { name: 'Morty Smith', status: 'Alive' },
   { name: 'Summer Smith', status: 'Alive' },
   { name: 'Beth Smith', status: 'Alive' },
-  { name: 'Birdperson', status: 'Dead' }
+  { name: 'Birdperson', status: 'Dead' },
+  { name: 'Mr. Meeseeks', status: 'Dead' },
+  { name: 'Evil Morty', status: 'Alive' },
+  { name: 'Jerry Smith', status: 'Alive' },
+  { name: 'Squanchy', status: 'Alive' },
+  { name: 'Tammy Gueterman', status: 'Dead' },
+  { name: 'Jessica', status: 'Alive' },
+  { name: 'Mr. Poopybutthole', status: 'Alive' },
+  { name: 'Noob-Noob', status: 'Alive' },
+  { name: 'Unity', status: 'Alive' },
+  { name: 'Birdperson', status: 'Dead' },
+  { name: 'Mr. Meeseeks', status: 'Dead' },
+  { name: 'Evil Morty', status: 'Alive' },
 ];
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'characters', loadComponent: () => import('./modules/characters/characters.component').then(m => m.CharactersComponent),
+    },
+
+    { path: 'episodes', loadComponent: () => import('./modules/episodes/episodes.component').then(m => m.EpisodesComponent),
+    },
+
+    { path: 'locations', loadComponent: () => import('./modules/locations/locations.component').then(m => m.LocationsComponent),
+    }
+];
